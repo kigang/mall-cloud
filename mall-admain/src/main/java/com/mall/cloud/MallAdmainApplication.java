fd40ab657036ff81cd46461a78e256483ad2089b
qinqigang <qigang.qin@hand-china.com>
2021-04-25 17:34:44 +0800

    [ADD]项目结构

---
+++ mall-admain/src/main/java/com/mall/cloud/MallAdmainApplication.java
@@ -0,0 +1,13 @@
+package com.mall.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallAdmainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallAdmainApplication.class, args);
+    }
+
+}
